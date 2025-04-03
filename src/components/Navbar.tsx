e518c77742f5eafd23d8f559164237748ed8b949
iqboljon <abduvaliyeviqboljon99@gmail.com>
2025-04-03 10:29:46 +0500

    Primekit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { GiHamburgerMenu } from "react-icons/gi"
 import { NavLink } from "react-router-dom"
 
 function Navbar() {
@@ -7,7 +8,7 @@ function Navbar() {
                 <div className="logo">
                     <h3 className="text-3xl font-bold">Prime<span className="text-[#008AFF]">Kit</span></h3>
                 </div>
-                <div className="flex gap-10 items-center">
+                <div className="hidden gap-10 items-center lg:flex">
                     <nav className="flex gap-10">
                         <NavLink className={({ isActive }) => `text-[18px] font-bold hover:text-[#008AFF] ${isActive ? "text-[#008AFF]" : "text-[#222D39]"}`} to={'/'}>Start</NavLink>
                         <NavLink className={({ isActive }) => `text-[18px] font-bold hover:text-[#008AFF] ${isActive ? "text-[#008AFF]" : "text-[#222D39]"}`} to={'/about'}>About</NavLink>
@@ -16,6 +17,9 @@ function Navbar() {
                         <NavLink className={({ isActive }) => `text-[18px] font-bold hover:text-[#008AFF] ${isActive ? "text-[#008AFF]" : "text-[#222D39]"}`} to={'/contact'}>Contact</NavLink>
                     </nav>
                     <button className="border-2 border-[#008AFF] py-2 px-4 font-bold rounded-lg text-[#fff] bg-[#008AFF] hover:border-black hover:bg-transparent hover:text-[black]">Get Started Now</button>
+                </div>
+                <div className="flex  lg:hidden text-2xl font-bold">
+                    <GiHamburgerMenu />
                 </div>
 
             </div>
